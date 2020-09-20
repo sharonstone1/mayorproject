9a11b653a12824d191aebd4a0186117e65b43886
sharonstone1 <marthe.ekoule@gmail.com>
2020-09-20 21:21:35 +0100

    backend: Update static rendering to ensure csrf token cookie is passed.

--- frontend_django/views.py
+++ frontend_django/views.py
@@ -1,8 +1,18 @@
 from django.conf import settings
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
 from django.views.generic.base import TemplateView
 
 
+from django.contrib.auth import authenticate, login, logout
+
+
 class IndexTemplateView(TemplateView):
+    @method_decorator(ensure_csrf_cookie)
+    def get(self, request, *args, **kwargs):
+        return super().get(request, args, kwargs)
+
     def get_template_names(self):
         template_name = "index.html"
         return template_name
