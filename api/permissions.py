27b7ff7968444490ca72caed11e521ee244a762d
sharonstone1 <marthe.ekoule@gmail.com>
2020-09-20 21:10:09 +0100

    backend: Create booking permission.     Permissions for any type of booking are as follow:         - anyone can book (authenticated or not)         - authenticate users can list elements, the view should take care of printing elements           belonging to the user.         - Retrieve, Update and Delete are reserved to owner and staff

---
+++ api/permissions.py
@@ -0,0 +1,29 @@
+from rest_framework import permissions
+
+
+class BookingPermission(permissions.BasePermission):
+    """
+    Permissions for any type of booking are as follow:
+        - anyone can book (authenticated or not)
+        - authenticate users can list elements, the view should take care of printing elements
+          belonging to the user.
+        - Retrieve, Update and Delete are reserved to owner and staff
+    """
+
+    def has_permission(self, request, view):
+        if request.method in ['POST', 'OPTIONS']:
+            return True
+
+        if request.user and request.user.is_authenticated:
+            return True
+        else:
+            return False
+
+    def has_object_permission(self, request, view, obj):
+        if obj.owner and obj.owner == request.user:
+            return True
+
+        if request.user.is_staff:
+            return True
+        else:
+            return False
