afee4ac513a6414a2b6758d4fbcb0a65d5282670
sharonstone1 <marthe.ekoule@gmail.com>
2020-09-22 21:13:25 +0100

    Frontend: Add text area control

---
+++ frontend/src/components/form/TextArea.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="form-group row">
+    <label :for="id" class="col-sm-2 col-form-label">
+      {{ label }}
+    </label>
+    <div class="col-sm-10">
+      <textarea class="form-control" :id="id"
+                :value="value"
+                @input="$emit('input', $event.target.value)">
+      </textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'textArea',
+  props: ['label', 'id', 'type', 'value']
+}
+</script>
+
+<style scoped>
+
+</style>
